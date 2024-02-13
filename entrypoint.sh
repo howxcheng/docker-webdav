55eb84e6e95de4007cf4b480ae339ef9a518f706
Howxcheng <chenghow3323@gmail.com>
2024-02-13 20:21:42 +0800

    update user

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,14 @@ else
     sed -i 's%auth_basic_user_file webdavpasswd;% %g' /etc/nginx/nginx.conf
 fi
 
+if [ -n "$(cat /etc/passwd | grep $PUID)" ]; then
+    old_name=$(cat /etc/passwd | awk -F: '{print $1,$3}' | grep $PUID | awk -F' ' '{print $1}')
+    usermod -l webdav $old_name
+    usermod -g $PGID webdav
+else
+    useradd -u $PUID -g $PGID -M webdav
+fi
+
 if [ -n "${PUID:-}" ] && [ -n "${PGID:-}" ]; then
     chmod go+w /dev/stderr /dev/stdout
     gosu $PUID:$PGID mkdir -p /media/.tmp
